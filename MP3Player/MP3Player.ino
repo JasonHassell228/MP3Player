59f5bec0dba6e98ae226f3104ceac91d5dcd458f
Jason Hassell <>
2026-07-30 21:08:32 +0100

    Switch code to BleKeyboard

--- MP3Player/MP3Player.ino
+++ MP3Player/MP3Player.ino
@@ -1,27 +1,35 @@
 
-#include "BluetoothSerial.h"
+// #include "BluetoothSerial.h"
 #include <BleKeyboard.h>
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
+// #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+// #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+// #endif
 
-BluetoothSerial SerialBT;
+// BluetoothSerial SerialBT;
+BleKeyboard bleKeyboard;
 const int playPauseButtonPin = 19;
+
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial.println("Starting BLE Work!");
   pinMode(playPauseButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
+  if (bleKeyboard.isConnected())
+  {
+    Serial.println("Connected to BT!!")
+    delay(1000);
+
+    Serial.println("Sending enter..")
+    bleKeyboard.write(KEY_RETURN);
+    delay(1000);
+
+    Serial.println("Sending play/pause..")
+
+    delay(1000);
   }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  Serial.println(digitalRead(playPauseButtonPin));
+
   delay(200);
 }
