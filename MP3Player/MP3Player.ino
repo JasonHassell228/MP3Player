4cb0d14766637e7e490f1115d4a03a8e31a66dc0
Jason Hassell <>
2026-08-30 09:24:29 +0100

    remove unneeded libs

--- MP3Player/MP3Player.ino
+++ MP3Player/MP3Player.ino
@@ -1,13 +1,11 @@
-
 // #include "BluetoothSerial.h"
-#include <BleKeyboard.h>
 
 // #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 // #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 // #endif
 
 // BluetoothSerial SerialBT;
-BleKeyboard bleKeyboard;
+
 const int playPauseButtonPin = 19;
 
 void setup() {
@@ -17,19 +15,18 @@ void setup() {
 }
 
 void loop() {
-  if (bleKeyboard.isConnected())
-  {
-    Serial.println("Connected to BT!!")
+  // if (bleKeyboard.isConnected())
+  // {
+    Serial.println("Connected to BT!!");
     delay(1000);
 
-    Serial.println("Sending enter..")
-    bleKeyboard.write(KEY_RETURN);
+    Serial.println("Sending enter..");
     delay(1000);
 
-    Serial.println("Sending play/pause..")
+    Serial.println("Sending play/pause..");
 
     delay(1000);
-  }
+  // }
 
   delay(200);
 }
