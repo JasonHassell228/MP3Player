9ecca7e465f547dd9e42451a0206961ef6fffb28
Jason Hassell <>
2026-09-03 15:04:24 +0100

    Add basic text display

--- MP3Player/MP3Player.ino
+++ MP3Player/MP3Player.ino
@@ -2,27 +2,26 @@
 #include <Wire.h>
 #include "SSD1306Wire.h"
 
-HijelHID_BLEKeyboard keyboard;
-// SSD1306Wire display(0x3c, SCREEN_SDA_PIN, SCREEN_SCK_PIN, GEOMETRY_128_32);
-
 const int PLAY_PAUSE_PIN = 18;
 const int VOL_DOWN_PIN = 5;
 const int VOL_UP_PIN = 19;
-// const int SCREEN_SDA_PIN = 23;
-// const int SCREEN_SCK_PIN = 22;
-const int numPins = 3;
+const int numBtnPins = 3;
 
-const int ALL_PINS[numPins] = {
+const int SCREEN_SDA_PIN = 23;
+const int SCREEN_SCK_PIN = 22;
+
+const int ALL_BTN_PINS[numBtnPins] = {
     PLAY_PAUSE_PIN,
     VOL_DOWN_PIN,
-    VOL_UP_PIN,
-    // SCREEN_SDA_PIN,
-    // SCREEN_SCK_PIN
+    VOL_UP_PIN
     };
+
+HijelHID_BLEKeyboard keyboard;
+SSD1306Wire display(0x3c, SCREEN_SDA_PIN, SCREEN_SCK_PIN, GEOMETRY_128_32);
 
 void SetPullupPins(const int pins[])
 {
-    for (int i = 0; i < numPins; i++)
+    for (int i = 0; i < numBtnPins; i++)
     {
         pinMode(pins[i], INPUT_PULLUP);
     }
@@ -30,26 +29,23 @@ void SetPullupPins(const int pins[])
 
 void setup() {
     Serial.begin(115200);
-
-    // pinMode(PLAY_PAUSE_PIN, INPUT_PULLUP);
-    // pinMode(VOL_DOWN_PIN, INPUT_PULLUP);
-    // pinMode(VOL_UP_PIN, INPUT_PULLUP);
-    SetPullupPins(ALL_PINS);
+    SetPullupPins(ALL_BTN_PINS);
 
     keyboard.setLogLevel(HIDLogLevel::Normal);
     keyboard.begin();
 
     // TODO Test why this doesn't work \/\/\/\/
     // ----- DISPLAY TEST -----
-    //     display.init();
-    //     display.setFont(ArialMT_Plain_10);
-    //     // loop
-    //     display.clear();
-    //     display.setTextAlignment(TEXT_ALIGN_RIGHT);
-    //     display.drawString(0, 0, "test");
-    //     // write the buffer to the display
-    //     display.display();
-    //     // todo loop
+        display.init();
+        display.flipScreenVertically();
+        display.setFont(ArialMT_Plain_24);
+        // loop
+        display.clear();
+        display.setTextAlignment(TEXT_ALIGN_LEFT);
+        display.drawString(0, 0, "Left aligned (0,0) blah blah");
+        // write the buffer to the display
+        display.display();
+        // todo loop
 
     Serial.println("Ready. Pair via Bluetooth settings, then press BOOT to type.");
 }
@@ -63,7 +59,7 @@ void loop() {
     //     }
     //     return;
     // }
-    SetPullupPins(ALL_PINS);
+    SetPullupPins(ALL_BTN_PINS);
     if (digitalRead(VOL_UP_PIN) == LOW) {
         keyboard.tap(MEDIA_VOLUME_UP, 25, 50);
         keyboard.releaseAll();
